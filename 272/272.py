e80ee937173968857730452a76d35abb4d62f4c1
Carolina <carolin861@gmail.com>
2019-07-22 18:44:19 +0200

    272 submission accepted

--- 272/272.py
+++ 272/272.py
@@ -1,23 +1,37 @@
-FIRST_TYPE = "aaaa"
-SECOND_TYPE = "'sssss'"
+# import inbuilt standard input output
+from sys import stdin, stdout
+
+FIRST_TYPE = "``"
+SECOND_TYPE = "''"
 MATCH = '"'
+FLAG = 0
+
+
+# suppose a function called main() and
+# all the operations are performed
+def main():
+    # for read and iterate all the lines
+    for line in stdin:
+        # print with stdout.write method the string parsed line
+        stdout.write(format_text(line))
+    # call the main method
 
 
 def format_text(text):
-    counter = 0
+    global FLAG
     new_text = ''
-    for character in text:
-        if character == MATCH:
-            counter += 1
-            if counter % 2 == 0:
-                character = SECOND_TYPE
+    for c in text:
+        if c == MATCH:
+            if FLAG == 0:
+                c = FIRST_TYPE
+                FLAG = 1
             else:
-                character = FIRST_TYPE
-        new_text += character
-    return text
+                c = SECOND_TYPE
+                FLAG = 0
+        new_text += c
+    return new_text
 
 
-if __name__ == '__main__':
-    s = input()
-    result = format_text(s)
-    print(result)
+if __name__ == "__main__":
+    main()
+
