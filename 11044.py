47b96a0057b9cc6ba84e599ce3958a98c0023959
Carolina <carolin861@gmail.com>
2019-07-23 21:08:55 +0200

    11044 solved

---
+++ 11044.py
@@ -0,0 +1,35 @@
+# import inbuilt standard input output
+from sys import stdin, stdout
+
+
+# suppose a function called main() and
+# all the operations are performed
+def main():
+    # for read and iterate all the lines
+    test_amount = int(input())
+    for test in range(test_amount):
+        n, m = map(int, input().split(" "))
+        # print with stdout.write method the string parsed line
+        print(get_sonars_amount(n, m))
+    # call the main method
+
+
+def get_sonars_amount(n, m):
+    # We do not need the border to be covered
+    n = n - 2
+    m = m - 2
+
+    while n % 3 != 0:
+        n += 1
+
+    while m % 3 != 0:
+        m += 1
+
+    n_slot = n / 3
+    m_slot = m / 3
+
+    return int(n_slot * m_slot)
+
+
+if __name__ == "__main__":
+    main()
