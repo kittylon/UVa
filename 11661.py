444133686025be6298ee6fe8a79664f20d9b3321
Carolina <carolin861@gmail.com>
2019-07-25 18:07:59 +0200

    11661 solved

--- 11661.py
+++ 11661.py
@@ -1,32 +1,36 @@
 # suppose a function called main() and
 # all the operations are performed
-from itertools import islice
 from sys import stdin
 
 
 def main():
-    for _ in stdin:
-        h_len = islice(stdin, None, None, 2)
-        highway = next(h_len)
+    for line in stdin:
+        h_len = int(line.strip())
+        if h_len:
+            highway = input()
 
-        if 'Z' in highway:
-            print(0)
-        else:
-            is_sort_asc(highway)
+            if 'Z' in highway:
+                print(0)
+            else:
+                is_sort_asc(highway)
 
 
 def is_sort_asc(highway):
     r_pos = 0
+    r_flag = 0
     d_pos = 0
+    d_flag = 0
     dis = []
     for pos, item in enumerate(highway):
         if item == 'R':
             r_pos = pos
-            if d_pos != 0:
+            r_flag = 1
+            if d_flag != 0:
                 dis.append(abs(r_pos - d_pos))
         elif item == 'D':
             d_pos = pos
-            if r_pos != 0:
+            d_flag = 1
+            if r_flag != 0:
                 dis.append(abs(r_pos - d_pos))
     if len(dis):
         print(min(dis))
